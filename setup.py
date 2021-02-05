ce8dc8043a01cc85de615a1b5ff8d043ab4853a8
Josef Schlehofer <pepe.schlehofer@gmail.com>
2021-02-05 14:48:13 +0100

    Bump v1.2.1
    * Add repository info & description
    * Update translations
    * Remove duplicated file for Norwegian Bokmål
    * Update snapshots
    * NPM audit fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class SnapshotsBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='1.2.0',
+    version='1.2.1',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
