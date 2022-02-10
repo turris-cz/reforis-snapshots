1b68dc435b891546dac3ba36c2f93fd1834e068a
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-02-10 11:23:28 +0100

    Fix empty table when no snapshots found

--- js/src/snapshots/SnapshotsTable/SnapshotsTable.js
+++ js/src/snapshots/SnapshotsTable/SnapshotsTable.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 CZ.NIC z.s.p.o. (http://www.nic.cz/)
+ * Copyright (C) 2019-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -24,34 +24,44 @@ export default function SnapshotsTable({
     deleteSnapshot,
 }) {
     return (
-        <div className="table-responsive">
-            <table className="table table-hover snapshots-table">
-                <thead className="thead-light">
-                    <tr>
-                        <th scope="col" className="text-center">
-                            {_("#")}
-                        </th>
-                        <th scope="col">{_("Description")}</th>
-                        <th scope="col">{_("Created at")}</th>
-                        <th scope="col">{_("Size")}</th>
-                        <th scope="col" aria-label={_("Actions")} />
-                    </tr>
-                </thead>
-                <tbody>
-                    {snapshots.map((snapshot) => (
-                        <SnapshotRow
-                            key={snapshot.number}
-                            snapshot={snapshot}
-                            rollbackSnapshot={() =>
-                                rollbackSnapshot(snapshot.number)
-                            }
-                            deleteSnapshot={() =>
-                                deleteSnapshot({ suffix: snapshot.number })
-                            }
-                        />
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <>
+            {snapshots.length === 0 ? (
+                <p className="text-muted text-center">
+                    {_("No snapshots found.")}
+                </p>
+            ) : (
+                <div className="table-responsive">
+                    <table className="table table-hover snapshots-table">
+                        <thead className="thead-light">
+                            <tr>
+                                <th scope="col" className="text-center">
+                                    {_("#")}
+                                </th>
+                                <th scope="col">{_("Description")}</th>
+                                <th scope="col">{_("Created at")}</th>
+                                <th scope="col">{_("Size")}</th>
+                                <th scope="col" aria-label={_("Actions")} />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {snapshots.map((snapshot) => (
+                                <SnapshotRow
+                                    key={snapshot.number}
+                                    snapshot={snapshot}
+                                    rollbackSnapshot={() =>
+                                        rollbackSnapshot(snapshot.number)
+                                    }
+                                    deleteSnapshot={() =>
+                                        deleteSnapshot({
+                                            suffix: snapshot.number,
+                                        })
+                                    }
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+        </>
     );
 }
